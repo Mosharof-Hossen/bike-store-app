26c4fa2a7d03530792a64ccd93864b61ee50c7bb
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2025-01-27 01:51:16 +0600

    ShortFeature added

--- bike-store-client/src/pages/Home/Home.tsx
+++ bike-store-client/src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import Banner from "../../components/Banner/Banner";
+import ShortFeature from "../../components/ShortFeature/ShortFeature";
 
 const Home = () => {
     return (
         <div className="">
             <Banner></Banner>
+            <ShortFeature></ShortFeature>
         </div>
     );
 };
