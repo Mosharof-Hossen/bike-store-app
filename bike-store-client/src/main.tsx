98775d7eaf4a7017d767bb9df698a124582690b8
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2025-01-26 19:40:32 +0600

    user info stored in localstoreage

--- bike-store-client/src/main.tsx
+++ bike-store-client/src/main.tsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.ts'
+import { persistor, store } from './redux/store.ts'
 import { RouterProvider } from 'react-router-dom'
 import router from './routers/Router.tsx'
 import { Toaster } from 'sonner'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}></RouterProvider>
+      <PersistGate loading = {null} persistor={persistor}>
+        <RouterProvider router={router}></RouterProvider>
+      </PersistGate>
       <Toaster position="top-right" ></Toaster>
     </Provider>
   </StrictMode>,
--- bike-store-client/src/redux/store.ts
+++ bike-store-client/src/redux/store.ts
@@ -1,22 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { baseApi } from './api/baseApi'
 import storage from 'redux-persist/lib/storage'
-import { persistReducer } from 'redux-persist'
 import { authReducer } from './features/Auth/authSlice'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
 
 const persistConfig = {
   key: 'auth',
   storage,
 }
-// const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
-    auth: authReducer
+    auth: persistedReducer,
   },
-  middleware: (defaultMiddleware) => defaultMiddleware().concat(baseApi.middleware)
+  middleware: (defaultMiddleware) => defaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    }
+  }).concat(baseApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+
+export const persistor = persistStore(store);
