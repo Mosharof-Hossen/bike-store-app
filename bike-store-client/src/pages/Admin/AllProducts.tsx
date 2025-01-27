5a120214ef85627c3e10db2fb67bf128472347e4
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2025-01-27 23:48:36 +0600

    product table done

--- bike-store-client/src/pages/Admin/AllProducts.tsx
+++ bike-store-client/src/pages/Admin/AllProducts.tsx
@@ -1,10 +1,11 @@
+import { FaEdit, FaTrash } from 'react-icons/fa';
 import SectionTitle from '../../components/SectionTitle/SectionTitle';
 import { useGetAllProductsQuery } from '../../redux/features/products/product.api';
 import { TMeta } from '../../types/global.type';
 import { TProduct } from '../../types/productsType';
 
 const AllProducts = () => {
-    const { data:meta,isFetching:metaFetching } = useGetAllProductsQuery(undefined,) as {
+    const { data: meta, isFetching: metaFetching } = useGetAllProductsQuery(undefined,) as {
         data?: {
             data?: TProduct[],
             meta?: TMeta;
@@ -12,7 +13,7 @@ const AllProducts = () => {
         isFetching: boolean;
     };
 
-    const { data, isFetching, } = useGetAllProductsQuery({ limit: meta?.meta?.total },{skip:metaFetching}) as {
+    const { data, isFetching, } = useGetAllProductsQuery({ limit: meta?.meta?.total }, { skip: metaFetching }) as {
         data?: {
             data?: TProduct[],
             meta?: TMeta;
@@ -20,7 +21,10 @@ const AllProducts = () => {
         isFetching: boolean;
     };
 
-    console.log(" data:",data?.data);
+    console.log(" data:", data?.data);
+
+    const handleDelete = (id:string) => { }
+    const handleItemEdit = (id:string) => { }
 
     return (
         <div className='px-10 space-y-10'>
@@ -36,19 +40,38 @@ const AllProducts = () => {
                         <thead>
                             <tr>
                                 <th></th>
-                                <th>Name</th>
-                                <th>Job</th>
-                                <th>Favorite Color</th>
+                                <th>Image</th>
+                                <th>Brand</th>
+                                <th>Category</th>
+                                <th>Model</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
                             </tr>
                         </thead>
                         <tbody>
                             {/* row 1 */}
-                            <tr>
-                                <th>1</th>
-                                <td>Cy Ganderton</td>
-                                <td>Quality Control Specialist</td>
-                                <td>Blue</td>
-                            </tr>
+                            {
+                                data?.data?.map((item, i) => <tr key={item._id}>
+                                    <th>{i + 1}</th>
+                                    <th>
+                                        <div className="avatar">
+                                            <div className="w-14 rounded-xl">
+                                                <img src={item.image} />
+                                            </div>
+                                        </div>
+                                    </th>
+                                    <td>{item.brand}</td>
+                                    <td>{item.category}</td>
+                                    <td>{item.model}</td>
+                                    <td>$ {item.price}</td>
+                                    <td>{item.quantity}</td>
+                                    {/* <td><button onClick={() => handleItemView(item)} className='flex items-center '><FaEye className='text-2xl text-green-500' /></button></td> */}
+                                    <td><button className='cursor-pointer' onClick={() => handleItemEdit(item._id)}><FaEdit className="text-2xl text-primary-c"></FaEdit></button></td>
+                                    <td><button className='cursor-pointer' onClick={() => handleDelete(item._id)}><FaTrash className="text-2xl text-red-500"></FaTrash></button></td>
+                                </tr>)
+                            }
                         </tbody>
                     </table>
                 </div>
