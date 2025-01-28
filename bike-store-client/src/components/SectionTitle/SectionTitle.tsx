24e4441c9e7927b0551e3beef046913357e940ab
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2025-01-29 03:16:49 +0600

    my order section done

--- bike-store-client/src/components/SectionTitle/SectionTitle.tsx
+++ bike-store-client/src/components/SectionTitle/SectionTitle.tsx
@@ -1,7 +1,7 @@
 
 const SectionTitle = ({ heading, subHeading }:{heading:string, subHeading:string }) => {
     return (
-        <div className='text-center space-y-5 p-8'>
+        <div className='text-center space-y-5 p-4'>
             <h2 className='md:text-4xl text-3xl text-[#22292f] font-bold '>{heading}</h2>
             <p className='text-base text-gray-500'>{subHeading}</p>
         </div>
--- bike-store-client/src/pages/User/MyOrders.tsx
+++ bike-store-client/src/pages/User/MyOrders.tsx
@@ -6,32 +6,57 @@ import { useGetUserOwnOrdersQuery } from '../../redux/features/Order/Order.api';
 
 const MyOrders = () => {
     const { data, isFetching } = useGetUserOwnOrdersQuery(undefined);
-    console.log({data});
+    console.log({ data });
     return (
-        <div className='px-10 space-y-10'>
-            <SectionTitle heading={"Control Your Inventory"} subHeading={"Add, edit, and manage your bike listings effortlessly."}></SectionTitle>
+        <div className='px-10 space-y-5'>
+            <SectionTitle heading={"Track Your Orders"} subHeading={"Check your order details, status, and delivery updates easily."}></SectionTitle>
             {
                 isFetching ?
                     <CustomSpinner></CustomSpinner>
                     :
-                    <div className="bg-white p-5 rounded">
+                    <div className="bg-white p-5 rounded space-y-8 text-sm">
 
 
                         {
 
-                            (data?.data as TUserOwnOrder[])?.map((item) => <div key={item._id}>
+                            (data?.data as TUserOwnOrder[])?.map((item,) => <div
+                                key={item._id}
 
-                                <div>
-                                    <div>
-                                        <h3 className='text-xl font-bold'>Customer information:</h3>
-                                        <p><span className='font-semibold'>User Id: </span>{item.user}</p>
-                                        <p><span className='font-semibold'>Order Date: </span>{moment(item.createdAt).format('YYYY-MM-DD h:mm:ss A')}</p>
-                                        <p><span className='font-semibold'>Last Update: </span>{moment(item.createdAt).format('YYYY-MM-DD h:mm:ss A')}</p>
+                            >
+                                <div className='grid md:grid-cols-2 grid-cols-1 gap-4'>
+                                    <div className='space-y-4'>
+                                        <div className='space-y-1'>
+                                            <h3 className='text-xl font-bold'>Customer information:</h3>
+                                            <p><span className='font-semibold'>User Id: </span>{item.user}</p>
+                                            <p><span className='font-semibold'>Order Date: </span>{moment(item.createdAt).format('YYYY-MM-DD h:mm:ss A')}</p>
+                                            <p><span className='font-semibold'>Last Update: </span>{moment(item.createdAt).format('YYYY-MM-DD h:mm:ss A')}</p>
+                                        </div>
+                                        <div className='space-y-1'>
+                                            <h3 className='text-xl font-bold'>Products:</h3>
+                                            {
+                                                item.products.map((product) => <div key={product._id}>
+                                                    <p><span className='font-semibold'>Product Id: </span>{product.product}, <span className='font-semibold'>Quantity: </span> {product.quantity}</p>
+                                                </div>)
+                                            }
+                                        </div>
                                     </div>
-                                    <div>
-                                        <h3 className='text-xl font-bold'>Products:</h3>
+
+                                    <div className='space-y-4'>
+                                        <div className='space-y-1'>
+                                            <h3 className='text-xl font-bold'>Order Summary:</h3>
+                                            <p><span className='font-semibold'>Total Price: </span>{item.totalPrice}</p>
+                                            <p className=''><span className='font-semibold'>Status: </span>{item.status}</p>
+                                        </div>
+                                        <div className='space-y-1'>
+                                            <h3 className='text-xl font-bold'>Transaction Details:</h3>
+                                            <p><span className='font-semibold'>Transaction Id: </span>{item.transaction.id}</p>
+                                            <p><span className='font-semibold'>Payment Method: </span>IBanking</p>
+                                            <p><span className='font-semibold'>Transaction Date: </span>{moment(item.updatedAt).format('YYYY-MM-DD h:mm:ss A')}</p>
+                                        </div>
                                     </div>
                                 </div>
+                                <div className="divider"></div>
+
                             </div>
                             )
                         }
